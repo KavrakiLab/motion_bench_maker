7496d1d33b09aac0702942cb29b1fd301c659b95
ChamzasKonstantinos <chamzask@gmail.com>
2022-07-25 02:39:51 +0000

    Moved docker back to the lower level

---
+++ docker/noetic-src/build-docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd "${0%/*}"
+docker build -f Dockerfile -t motion_bench_maker_moveit_src ../../
--- noetic-src/build-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd "${0%/*}"
-docker build -f Dockerfile -t motion_bench_maker_moveit_src ../
